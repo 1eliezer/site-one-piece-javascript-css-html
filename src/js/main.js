681613325ecea1300f29df830c8fc4c1d3da2b50
Eliézer <1elliezer@gmail.com>
2023-10-03 21:56:52 -0300

    first commit aqui

---
+++ src/js/main.js
@@ -0,0 +1,107 @@
+document.querySelector('#app').innerHTML = `
+    <div class ="personagem">
+        <img src="./src/img/zoro.png" class="imagem" alt="Roronoa Zoro | One Piece">
+        <div class="conteudo">
+            <i class="logo"></i> 
+            <h2 class="nome-personagem">Roronoa Zoro</h2>
+                <p class="descricao">
+                    Roronoa Zoro, é um personagem fictício da série
+                    One Piece criada por Eiichiro Oda. Na história,
+                    Zoro era um caçador de piratas que por fim se 
+                    torna um quando é convencido pelo protagonista 
+                    Monkey D. Luffy a ser o primeiro membro de sua 
+                tripulação, os Piratas do Chapéu de Palha
+            </p>
+        </div>
+    </div>
+    <div class ="personagem">
+        <img src="./src/img/sanji.png" class="imagem" alt=" Vinsmoke Sanji | One Piece">
+            <div class="conteudo">
+                <i class="logo"></i>
+                <h2 class="nome-personagem"> Vinsmoke Sanji</h2>
+                <p class="descricao">
+                Vinsmoke Sanji é um personagem criado por Eiichiro Oda
+                 para o seu mangá e anime One Piece. Ele é introduzido 
+                 ainda na primeira saga da história como um cozinheiro 
+                 do restaurante-navio Baratie e depois passa a trabalhar
+                  para o capitão Monkey D. Luffy no seu bando dos Piratas
+                 do Chapéu de Palha.
+                </p>
+        </div>
+    </div>
+    <div class ="personagem selecionado">
+         <img src="./src/img/luffy.png" class="imagem" alt=" Monkey D. Luffy | One Piece">
+            <div class="conteudo">
+                <i class="logo"></i>
+                <h2 class="nome-personagem"> Monkey D. Luffy</h2>
+                <p class="descricao">
+                Monkey D. Luffy, é um personagem fictício e o protagonista 
+                da franquia One Piece criada por Eiichiro Oda. Ele é 
+                apresentado como um jovem cujo corpo ganha as propriedades 
+                de borracha após ter comido a Gomu-Gomu no Mi, uma das 
+                várias frutas amaldiçoadas conhecidas como Akuma no Mi 
+                ou Frutas do Diabo.
+                </p>
+        </div>
+    </div>
+        <div class ="personagem">
+         <img src="./src/img/nami.png" class="imagem" alt="Nami | One Piece">
+            <div class="conteudo">
+                <i class="logo"></i>
+                <h2 class="nome-personagem">Nami</h2>
+                <p class="descricao">
+                Nami, é uma personagem fictícia do mangá e anime One Piece 
+                criada por Eiichiro Oda. Nami é introduzida na narrativa como
+                 uma aliada oportunista do protagonista Monkey D. Luffy mas 
+                 que possui motivos nobres por trás de suas ações ambíguas.
+                </p>
+        </div>
+    </div>
+        <div class ="personagem">
+         <img src="./src/img/chopper.png" class="imagem" alt="Tony Tony Chopper | One Piece">
+            <div class="conteudo">
+                <i class="logo"></i>
+                <h2 class="nome-personagem">Chopper</h2>
+                <p class="descricao">
+                Tony Tony Chopper, também conhecido como "Amante de Algodão Doce", 
+                é um personagem fictício da série de mangá One Piece criada por 
+                Eiichiro Oda. Ele é o sexto membro a se juntar e o médico dos 
+                Piratas do Chapéu de Palha. Chopper é freqüentemente apresentado 
+                em mercadorias oficiais, servindo como mascote da série.
+                </p>
+        </div>
+    </div>
+    
+   
+`
+document.querySelector('#app-menu').innerHTML = `
+        <li>
+			<button class="botao">
+			<div class="imglg logo-one-piece-1 logo-wh"></div>
+			</button>
+		</li>
+		<li>
+			<button class="botao">
+				<div class="imglg logo-one-piece-2 logo-wh"></div>
+		
+			</button>
+		</li>
+		<li>
+			<button class="botao selecionado">
+				<div class="imglg logo-one-piece-3 logo-wh"></div>
+		
+			</button>
+		</li>
+		<li>
+			<button class="botao">
+				<div class="imglg logo-one-piece-4 logo-wh"></div>
+		
+			</button>
+		</li>
+		<li>
+			<button class="botao">
+				<div class="imglg logo-one-piece-5 logo-wh"></div>
+		
+			</button>
+		</li>
+`
